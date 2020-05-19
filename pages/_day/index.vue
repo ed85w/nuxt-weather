f783b1618a7cdb93e8d9cfa816b2dc1df05fa5dd
ed85w <ed.walker@hotmail.co.uk>
2020-05-19 21:24:33 +0100

    single day page has single day's forecasts

--- pages/_day/index.vue
+++ pages/_day/index.vue
@@ -1,30 +1,45 @@
 <template>
   <div>
     <h1>Hello</h1>
-    <h1>{{ forecasts }}</h1>
+    <h1>{{ thisDayMidday[0].weather[0].main }}</h1>
+    <ul>
+      <li v-for="forecast in thisDayForecasts" :key="forecast.index">
+        {{ forecast.weather[0].main }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 
 export default {
-  props: {
-    // eslint-disable-next-line vue/require-default-prop
-    forecast: {
-      type: Object
-    },
-    day: {
-      type: String,
-      default: ''
-    },
-    date: {
-      type: String,
-      default: ''
-    }
-  },
+  // props: {
+  //   // eslint-disable-next-line vue/require-default-prop
+  //   forecast: {
+  //     type: Object
+  //   },
+  //   day: {
+  //     type: String,
+  //     default: ''
+  //   },
+  //   date: {
+  //     type: String,
+  //     default: ''
+  //   }
+  // },
   computed: {
     forecasts () {
       return this.$store.state.forecasts
+    },
+    thisDayForecasts () {
+      const forecasts = this.forecasts
+      const theDate = this.$route.params.day.slice(-10)
+      return forecasts.filter(forecast => forecast.dt_txt.slice(0, 10) === theDate)
+    },
+    thisDayMidday () {
+      const forecasts = this.forecasts
+      const theDate = this.$route.params.day.slice(-10)
+      return forecasts.filter(forecast => forecast.dt_txt.slice(0, 10) === theDate && forecast.dt_txt.slice(-8) === '12:00:00')
     }
   }
 }
